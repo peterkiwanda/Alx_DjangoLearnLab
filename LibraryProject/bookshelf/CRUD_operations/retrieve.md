6da0aa6e53be1d57ae6e31b1da515cf0adaec124
peterkiwanda <162957875+peterkiwanda@users.noreply.github.com>
2024-09-24 14:34:09 +0300

    Added Book model and CRUD operations for bookshelf app
    Added Book model and CRUD operations for bookshelf app

---
+++ LibraryProject/bookshelf/CRUD_operations/retrieve.md
@@ -0,0 +1,4 @@
+# Retrieve Book
+```python
+book = Book.objects.get(id=1)
+print(book.title, book.author, book.publication_year)
---
+++ LibraryProject/bookshelf/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+# Register the Book model with some custom admin configurations
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author__name')
